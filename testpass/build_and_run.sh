29b8e3382e87b5e3b23e4e3d8f994588a1300a96
Tomas Meszaros <exo@tty.sk>
2018-03-09 19:45:47 +0100

    added disassembly after basics passes

--- testpass/build_and_run.sh
+++ testpass/build_and_run.sh
@@ -5,12 +5,24 @@ cmake ..
 make
 cd ..
 
-#clang-format -style=google -i testpass/*.cpp
+clang-format -style=google -i testpass/*.cpp
 
-#clang -Xclang -load -Xclang build/skeleton/libSkeletonPass.* c_code/test.c
+
+# compile without anything
 clang -O0 -c -emit-llvm c-code/test.c -o build/test.bc
 
-opt -o build/test_after_opt.bc -mem2reg -dce -loop-simplify -simplifycfg -load build/testpass/libTestPass.so -testpass < build/test.bc > /dev/null
+
+# run some basic optmizations, so the IR is cleaner
+opt -o build/test_after_default_opt.bc -mem2reg -dce -loop-simplify -simplifycfg build/test.bc
+
+# run pass
+opt -o build/test_after_opt.bc -mem2reg -dce -loop-simplify -simplifycfg -load build/testpass/libTestPass.so -testpass < build/test_after_default_opt.bc > /dev/null
+
+# disassemble bytecode so I can look inside
+llvm-dis build/test.bc
+llvm-dis build/test_after_default_opt.bc
 llvm-dis build/test_after_opt.bc
-#lli build/test_after_opt.bc
+
+# run pass and execute modified bytecode
+lli build/test_after_opt.bc
 
