286470a5c6139e0540d59bb68ba082184bd42b74
Tomas Meszaros <exo@tty.sk>
2018-12-08 00:25:31 +0100

    apex launcher done

--- src/apex/apexlib.c
+++ src/apex/apexlib.c
@@ -11,31 +11,3 @@ void _apex_exit(int exit_code) {
 void _apex_extract_int(int i) {
   printf("%d", i);
 }
-/*
-void _apex_extract_int(int i) {
-  // Figure out number of digits @i has,
-  // so we can allocate big enough buffer.
-  int i_digits = 0;
-  if (i == 0) {
-    i_digits = 1;
-  } else {
-    i_digits = floor(log10(abs(i)))+1;
-  }
-  // Store @i as string into @i_string buffer.
-  char i_string[i_digits+2];
-  sprintf(i_string, "%d\n", i);
-
-  // Save @i_string into @APEX_OUTPUT
-  int status = 0;
-  FILE *f = fopen(APEX_OUTPUT, "w+");
-  if (f != NULL) {
-    if (fputs(i_string, f) != EOF) {
-      status = 1;
-    }
-    fclose(f);
-  }
-  if (status != 1) {
-    printf("Error: Could not save to file!\n");
-  }
-}
-*/
